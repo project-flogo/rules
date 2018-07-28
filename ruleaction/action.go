120514e94101fc3ae45452b598253d23c8d747cb
balamg <balamg@yahoo.com>
2018-07-28 16:07:49 +0530

    demonstrate a join in rules

--- ruleaction/action.go
+++ ruleaction/action.go
@@ -11,6 +11,8 @@ import (
 	"github.com/TIBCOSoftware/flogo-lib/core/action"
 	"github.com/TIBCOSoftware/flogo-lib/core/data"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
+	"strconv"
 )
 
 const (
@@ -76,11 +78,18 @@ func (a *RuleAction) Run(ctx context.Context, inputs map[string]*data.Attribute)
 
 	}()
 
-	streamTuple := model.NewStreamTuple("n1") //n1 -> will be replaced by contextual information coming in the data
+	h, _ok := trigger.HandlerFromContext(ctx)
+	if !_ok {
+		return nil, nil
+	}
+	fmt.Printf("Received event from stream source [%s]", h.Name)
+
+	streamSrc := model.StreamSource(h.Name)
+
+	streamTuple := model.NewStreamTuple(streamSrc) //n1 -> will be replaced by contextual information coming in the data
 
 
 	queryParams := inputs["queryParams"].Value().(map[string]string)
-
 
 
 	//map input data into stream tuples, only string. ignore the rest for now
@@ -93,31 +102,86 @@ func (a *RuleAction) Run(ctx context.Context, inputs map[string]*data.Attribute)
 }
 
 func loadRules(rs ruleapi.RuleSession) {
-	rule := ruleapi.NewRule("n1.name == Bob")
-	fmt.Printf("Rule added: [%s]\n", rule.GetName())
-	rule.AddCondition("c1", []model.StreamSource{"n1"}, checkForBob) // check for name "Bob" in n1
-	//in effect, fire the rule when name field in both tuples in "Bob"
-	rule.SetActionFn(myActionFn)
+
+	rule := ruleapi.NewRule("customer-event")
+	rule.AddCondition("customer", []model.StreamSource{"customerevent"}, truecondition) // check for name "Bob" in n1
+	rule.SetActionFn(customer)
+	rule.SetPriority(1)
 	rs.AddRule(rule)
+	fmt.Printf("Rule added: [%s]\n", rule.GetName())
+
+	rule2 := ruleapi.NewRule("debit-event")
+	rule2.AddCondition("debitevent", []model.StreamSource{"debitevent"}, truecondition)
+	rule2.SetActionFn(debit)
+	rule2.SetPriority(2)
+	rs.AddRule(rule2)
+	fmt.Printf("Rule added: [%s]\n", rule2.GetName())
+
+	rule3 := ruleapi.NewRule("customer-debit")
+	rule3.AddCondition("customerdebit", []model.StreamSource{"debitevent", "customerevent"}, customerdebitjoincondition)
+	rule3.SetActionFn(debitAction)
+	rule3.SetPriority(3)
+	rs.AddRule(rule3)
+	fmt.Printf("Rule added: [%s]\n", rule3.GetName())
+
+	//rule4 := ruleapi.NewRule("check-balance")
+	//rule4.AddCondition("customerdebit", []model.StreamSource{"customerevent"}, checkBalance) // check for name "Bob" in n1
+	//rule4.SetActionFn(balanceAlert)
+	//rule4.SetPriority(4)
+	//rs.AddRule(rule4)
+	//fmt.Printf("Rule added: [%s]\n", rule4.GetName())
+
+
 }
 
-func checkForBob(ruleName string, condName string, tuples map[model.StreamSource]model.StreamTuple) bool {
-	//This conditions filters on name="Bob"
-	streamTuple := tuples["n1"]
+
+func truecondition (ruleName string, condName string, tuples map[model.StreamSource]model.StreamTuple) bool {
+	return true
+}
+func customer(ruleName string, tuples map[model.StreamSource]model.StreamTuple) {
+	streamTuple := tuples["customerevent"]
 	if streamTuple == nil {
 		fmt.Println("Should not get a nil tuple in FilterCondition! This is an error")
-		return false
+	} else {
+		name := streamTuple.GetString("name")
+		fmt.Printf("Received a customer event with customer name [%s]\n", name)
 	}
-	name := streamTuple.GetString("name")
-	return name == "Bob"
+}
+func debit(ruleName string, tuples map[model.StreamSource]model.StreamTuple) {
+	streamTuple := tuples["debitevent"]
+	if streamTuple == nil {
+		fmt.Println("Should not get a nil tuple in FilterCondition! This is an error")
+	} else {
+		name := streamTuple.GetString("name")
+		amount := streamTuple.GetString("debit")
+
+		fmt.Printf("Received a debit event for customer [%s], amount [%s]\n", name, amount)
+	}
 }
 
-func myActionFn(ruleName string, tuples map[model.StreamSource]model.StreamTuple) {
-	fmt.Printf("Rule fired: [%s]\n", ruleName)
-	streamTuple1 := tuples["n1"]
-	if streamTuple1 == nil {
-		fmt.Println("Should not get nil tuples here in Action! This is an error")
+func customerdebitjoincondition(ruleName string, condName string, tuples map[model.StreamSource]model.StreamTuple) bool {
+
+	customerTuple := tuples["customerevent"]
+	debitTuple := tuples["debitevent"]
+
+	if customerTuple == nil || debitTuple == nil {
+		fmt.Println("Should not get a nil tuple here! This is an error")
+		return false
 	}
-	name1 := streamTuple1.GetString("name")
-	fmt.Printf("n1.name = [%s]", name1)
+	custName := customerTuple.GetString("name")
+	acctName := debitTuple.GetString("name")
+
+	return custName == acctName
+
+}
+
+func debitAction(ruleName string, tuples map[model.StreamSource]model.StreamTuple) {
+	//fmt.Printf("Rule fired: [%s]\n", ruleName)
+	customerTuple := tuples["customerevent"].(model.MutableStreamTuple)
+	debitTuple := tuples["debitevent"]
+	dbt := debitTuple.GetString("debit")
+	debitAmt, _ := strconv.ParseFloat(dbt, 64)
+	currBal := customerTuple.GetFloat("balance")
+	customerTuple.SetFloat("balance", customerTuple.GetFloat("balance") - debitAmt)
+	fmt.Printf("Customer [%s], Balance [%f], Debit [%f], NewBalance [%f]\n", customerTuple.GetString("name"), currBal, debitAmt, customerTuple.GetFloat("balance"))
 }
