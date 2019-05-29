7230f5bb08613d6d40da0be12b73fb9768786e91
LakshmiMekala <lmekala@tibco.com>
2019-05-29 11:57:43 +0530

    adding sanity test file for creditcard example

---
+++ examples/flogo/creditcard/sanity.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+function get_test_cases {
+    local my_list=( testcase1 testcase2 testcase3 )
+    echo "${my_list[@]}"
+}
+
+function testcase1 {
+pushd $GOPATH/src/github.com/project-flogo/rules/examples/flogo/creditcard/json
+flogo create -f flogo.json
+cp functions.go cardapp/src
+cd cardapp
+flogo build
+./bin/cardapp > /tmp/testcase1.log 2>&1 &
+pId=$!
+
+response=$(curl -X PUT http://localhost:7777/applicant -H 'Content-Type: application/json' -d '{"name":"Sam4","age":"26","salary":"50100","address":"SFO","id":"4"}' --write-out '%{http_code}' --silent --output /dev/null)
+response1=$(curl -X PUT http://localhost:7777/cibildata -H 'Content-Type: application/json' -d '{"id":"4","name":"Sam4","creditScore":"850"}' --write-out '%{http_code}' --silent --output /dev/null)
+kill -9 $pId
+
+if [ $response -eq 200 ] && [ $response1 -eq 200 ] && [[ "echo $(cat /tmp/testcase1.log)" =~ "Rule fired" ]]
+    then 
+        echo "PASS"
+    else
+        echo "FAIL"
+fi
+cd ..
+rm -rf cardapp
+popd
+}
+
+function testcase2 {
+pushd $GOPATH/src/github.com/project-flogo/rules/examples/flogo/creditcard/json
+flogo create -f flogo.json
+cp functions.go cardapp/src
+cd cardapp
+flogo build
+./bin/cardapp > /tmp/testcase2.log 2>&1 &
+pId=$!
+
+response=$(curl -X PUT http://localhost:7777/applicant -H 'Content-Type: application/json' -d '{"name":"Sam4","age":"26","salary":"50100","address":"SFO","id":"4"}' --write-out '%{http_code}' --silent --output /dev/null)
+response1=$(curl -X PUT http://localhost:7777/cibildata -H 'Content-Type: application/json' -d '{"id":"4","name":"Sam4","creditScore":"650"}' --write-out '%{http_code}' --silent --output /dev/null)
+
+kill -9 $pId
+if [ $response -eq 200  ] && [ $response1 -eq 200  ] && [[ "echo $(cat /tmp/testcase2.log)" =~ "lower cibil score" ]]
+    then 
+        echo "PASS"
+    else
+        echo "FAIL"
+fi
+cd ..
+rm -rf cardapp
+popd
+}
+
+function testcase3 {
+pushd $GOPATH/src/github.com/project-flogo/rules/examples/flogo/creditcard/api
+go run main.go > /tmp/testcase3.log 2>&1
+
+if [[ "echo $(cat /tmp/testcase3.log)" =~ "Rule fired" ]]
+    then 
+        echo "PASS"
+    else
+        echo "FAIL"
+fi
+popd
+}
