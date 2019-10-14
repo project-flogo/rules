60cd8c053339c480df22c7e6a87b5c480db8c050
Andrew Snodgrass <git@pointlander.info>
2019-10-14 13:30:54 -0600

    Added mutex to redis JtService

--- rete/internal/redis/rjtservice.go
+++ rete/internal/redis/rjtservice.go
@@ -1,6 +1,8 @@
 package redis
 
 import (
+	"sync"
+
 	"github.com/project-flogo/rules/common/model"
 	"github.com/project-flogo/rules/rete/internal/types"
 )
@@ -8,6 +10,7 @@ import (
 type jtServiceImpl struct {
 	types.NwServiceImpl
 	allJoinTables map[string]types.JoinTable
+	sync.RWMutex
 }
 
 func NewJoinTableCollection(nw types.Network, config map[string]interface{}) types.JtService {
@@ -21,10 +24,14 @@ func (jtc *jtServiceImpl) Init() {
 }
 
 func (jtc *jtServiceImpl) GetJoinTable(name string) types.JoinTable {
+	jtc.RLock()
+	defer jtc.RUnlock()
 	return jtc.allJoinTables[name]
 }
 
 func (jtc *jtServiceImpl) GetOrCreateJoinTable(nw types.Network, rule model.Rule, identifiers []model.TupleType, name string) types.JoinTable {
+	jtc.Lock()
+	defer jtc.Unlock()
 	jT, found := jtc.allJoinTables[name]
 	if !found {
 		jT = newJoinTableImpl(nw, rule, identifiers, name)
